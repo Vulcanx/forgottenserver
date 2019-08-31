b407d24e40aee1ad5f48179b39ea05805c5eb95a
Vulcanx <spaghetti90314@gmail.com>
2019-08-30 19:17:42 -0700

    Remove custom changes

--- data/global.lua
+++ data/global.lua
@@ -62,35 +62,3 @@ end
 if not nextUseStaminaTime then
 	nextUseStaminaTime = {}
 end
-function getfenv(fn)
-  local i = 1
-  while true do
-    local name, val = debug.getupvalue(fn, i)
-    if name == "_ENV" then
-      return val
-    elseif not name then
-      break
-    end
-    i = i + 1
-  end
-end
-
-
-function setfenv(fn, env)
-  local i = 1
-  while true do
-    local name = debug.getupvalue(fn, i)
-    if name == "_ENV" then
-      debug.upvaluejoin(fn, i, (function()
-        return env
-      end), 1)
-      break
-    elseif not name then
-      break
-    end
-
-    i = i + 1
-  end
-
-  return fn
-end
